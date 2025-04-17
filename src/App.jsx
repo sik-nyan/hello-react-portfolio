4c4a8feb91ed5eee82379e43422e2d3d85b2f5c9
sik-nyan <sik-nyan@example.com>
2025-04-17 23:01:25 +0900

    初回コミット

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <button onClick={() => alert("こんにちは！")}>クリックしてね</button>
+    </div>
+  );
+}
+
+export default App;
+
