3b1575d292b161f995f026814b7f4304c1a1349e
sik <s.shinzato.94@gmail.com>
2025-04-18 11:08:35 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename="/hello-react-portfolio">
       <App />
-    <BrowserRouter/>
+    </BrowserRouter>
   </StrictMode>,
 )
