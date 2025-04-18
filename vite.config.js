88b6f28006d6904feac6cad10c15814c23c44157
sik <s.shinzato.94@gmail.com>
2025-04-18 10:53:28 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hello-react-portfolio/', // ← ここを追加！
+  base: '/', // ← ここを追加！
   plugins: [react()],
 })
